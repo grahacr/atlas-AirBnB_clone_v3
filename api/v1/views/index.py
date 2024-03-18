2632c0d337f5137b6a1e419f712bacd3c13fb643
grahacr <courtney.graham@atlasschool.com>
2024-03-18 17:00:37 -0500

    jsonify response

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """module for checking status of route"""
-
-
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def index():
-    """"""
+    """return status"""
     response = {"status": "OK"}
-    return response
+    return jsonify(response)
