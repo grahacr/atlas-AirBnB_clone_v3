7ad932bec20aa155a94e4baf00e686f5593e55ae
grahacr <courtney.graham@atlasschool.com>
2024-03-18 17:23:58 -0500

    minor fixes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """New module for application"""
 
-
+from api.v1.views import app_views
 from flask import Flask, Blueprint
 from models import storage
-from api.v1.views import app_views
-import os
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close():
+def teardown(exception):
     storage.close()
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
     app.run(host=host, port=port, threaded=True)
