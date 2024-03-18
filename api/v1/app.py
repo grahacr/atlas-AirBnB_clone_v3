7db62fd7d9eac84c458aa6d587e0f228404e4804
grahacr <courtney.graham@atlasschool.com>
2024-03-18 16:02:04 -0500

    fix import and add init

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,10 @@
 """New module for application"""
 from flask import Flask, Blueprint
 from models import storage
-from api.v1.views app_views
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
