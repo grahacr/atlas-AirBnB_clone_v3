a2ea3b26f4218b18fe5e2be38e9abe75d887eac2
grahacr <courtney.graham@atlasschool.com>
2024-03-18 20:52:10 -0500

    add count function to return number of objects for each type of class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """module for checking status of route"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -9,3 +10,14 @@ def index():
     """return status"""
     response = {"status": "OK"}
     return response
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    objects_count = {}
+    objects_count['amenities'] = storage.count('Amenity')
+    objects_count['cities'] = storage.count('City')
+    objects_count['places'] = storage.count('Place')
+    objects_count['reviews'] = storage.count('Review')
+    objects_count['states'] = storage.count('State')
+    objects_count['users'] = storage.count('User')
+    return objects_count
