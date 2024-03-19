9af49d180e1e57c2f2496bdd34bc540058d070f1
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-18 23:30:41 -0500

    Adding some more methods to states file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
 """Handles all default RESTFul API actions"""
 from api.v1 import app
-from flask import abort, jsonify, Blueprint
-from .models import State
+from api.v1.views import app_views
+from flask import abort, jsonify, Blueprint, request
+from .models import State, BaseModel
 
 states_bp = Blueprint('states', __name__)
 
 
-@states_bp.route('/api/v1/states', methods=['GET'])
+@app.route('/api/v1/states', methods=['GET'])
 def get_all_states():
     states = State.query.all()
     return jsonify([state.to_dict() for state in states])
 
-@states_bp.route('/api/v1/states/<int:state_id>', methods=['GET'])
+@app.route('/api/v1/states/<int:state_id>', methods=['GET'])
 def get_state(state_id):
     state = State.query.get(state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
+@app.route('/api/v1/states/<int:state_id>', method=['DELETE'])
+def del_state(state_id):
+    if request.method == 'DELETE':
+        states = State.query.all()
+        for state in states:
+            if state.id == state_id:
+                state.delete()
+                return jsonify({}), 200
+        abort(404)
+@app.route('/api/v1/states', method=['POST'])
+def post_state():
+    
