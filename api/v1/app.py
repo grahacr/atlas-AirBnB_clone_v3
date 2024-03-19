aede3f873b66ba4154d55acff0c29cc141a6a3e7
grahacr <courtney.graham@atlasschool.com>
2024-03-18 20:36:52 -0500

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,9 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-@app.teardown_appcontext
 
+
+@app.teardown_appcontext
 def teardown(exception):
     storage.close()
 
