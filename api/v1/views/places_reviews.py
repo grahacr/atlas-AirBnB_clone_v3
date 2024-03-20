6bf150413c0ccb27c9e18e89506906fc414f07e0
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-20 17:41:52 -0500

    found the error

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -62,15 +62,15 @@ def post_review(place_id):
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id):
-    found_review = request.get_json(silent=True)
-    if not found_review:
-        abort(400)
     review_data = storage.get(Review, review_id)
     if not review_data:
         abort(404)
+    found_review = request.get_json(silent=True)
+    if not found_review:
+        abort(400)
     for key, value in found_review.items():
         if key not in [
             "id", "user_id", "place_id", "created_at", "updated_at"]:
             setattr(found_review, key, value)
-    review_data.save()
+    storage.save()
     return jsonify(review_data.to_dict()), 200
