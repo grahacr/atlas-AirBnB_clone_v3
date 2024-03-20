65c60b3029173fb46c1e4b9b10800dafa2942268
grahacr <courtney.graham@atlasschool.com>
2024-03-20 11:30:45 -0500

    fix underscore typo

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -15,7 +15,7 @@ def get_reviews(place_id):
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = [review.to.dict() for review in place.reviews]
+    reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
 
 
