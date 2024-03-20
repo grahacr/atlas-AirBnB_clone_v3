551f984259a2d3a6681244c9d00a5d3315c40c0f
grahacr <courtney.graham@atlasschool.com>
2024-03-20 11:08:54 -0500

    fix reviews variable

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -15,8 +15,8 @@ def get_reviews(place_id):
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = storage.all(Review).values()
-    return jsonify([review.to_dict() for review in place.reviews])
+    reviews = [review.to.dict() for review in place.reviews]
+    return jsonify(reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
